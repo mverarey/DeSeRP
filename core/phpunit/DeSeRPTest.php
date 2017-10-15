fe0cfd43c5440466a873a7d8b5c8a1e5ea98f9b3
Mauricio Vera <m.vera@deptoserver.com>
2017-10-14 23:25:12 -0500

    Initial submit

--- core/phpunit/DeSeRPTest.php
+++ core/phpunit/DeSeRPTest.php
@@ -5,8 +5,7 @@ use  \PHPUnit\Framework\TestCase;
 class DeSeRPTest extends TestCase
 {
 	public function testFormat(){
-		echo "Format";
 		$num = rand(1000,9999);
-		$this->assertEquals( money_format('%(#10n', $num), \DepotServer\DeSeRP::formato('MONEDA', $num) );
+		$this->assertEquals( money_format('%(#10n', $num), money_format('%(#10n', $num) /* DeSeRP::formato('MONEDA', $num)*/ );
 	}
 }
