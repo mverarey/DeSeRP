ce8164705141ac5a3a0d54933b4f36239ce2e528
Mauricio Vera <m.vera@deptoserver.com>
2017-10-14 20:41:23 -0500

    Initial submit

---
+++ classes/Configuracion.Inicial.class.php
@@ -0,0 +1,40 @@
+<?php namespace DepotServer;
+
+/**
+ * Configuración
+ *
+ * NOTA IMPORTANTE:
+ * Este archivo deberá ser configurado como SOLO-LECTURA durante la 
+ * instalación. Si hace cambios en el archivo, asegurese de protegerlo
+ * nuevamente despues de hacer los cambios.
+ *
+ * @access       public
+ * @author       Mauricio Vera <m.vera@depotserver.com>
+ * @copyright    DepotServer 2010
+ * @name         Configuracion
+ * @since		0.1_beta
+ * @var			array	servidor, usuario, password, db, debug
+ * @version		5.0
+ */
+class Configuracion_Inicial{
+
+	// Base de Datos	
+	var $servidor = "localhost";
+	var $usuario = "root";
+	var $password = "";
+	var $db = "tipi_proto";
+
+	// SMTP
+	var $smtp_host = "smtp.server.com";
+	var $smtp_port = "26";
+	var $smtp_auth = true;
+	var $smtp_usuario = "";
+	var $smtp_password = "";
+	var $smtp_persist = FALSE;
+	
+	// Generales
+	var $url_absoluta = "https://localhost";
+	var $locale = "es_MX";
+	var $debug = false;
+}
+?>
