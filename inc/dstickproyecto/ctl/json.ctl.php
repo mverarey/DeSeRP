10dde9aa0252f07e98cf3e39e8c3d0748f9b4fa6
Mauricio Vera <mail@mauriciovera.com>
2019-02-10 21:43:56 -0600

    Initial commit DSTickets

--- inc/dstickproyecto/ctl/json.ctl.php
+++ inc/dstickproyecto/ctl/json.ctl.php
@@ -1,8 +1,5 @@
 <?php
 $publica = false;
 $tabla = "dstick_proyecto";
-$campos = ["dstick_proyecto.id", "idLiderProyecto", "idCategoria", "idCliente", "dstick_proyecto.nombre", "descripcion", "fechaCreacion", "fechaInicio", "fechaFin", "estado"];
-
-$joins = [
-	[ "tabla" => "usuarios", "col_origen" => "idLiderProyecto",	"col_destino" => "id", "col_mostrar" => "usuarios.nombre"]
-];
+$campos = ["dstick_proyecto.id", "dstick_proyecto.idLiderProyecto", "dstick_proyecto.idCategoria", "dstick_proyecto.idCliente", "dstick_proyecto.nombre", "dstick_proyecto.descripcion", "dstick_proyecto.fechaCreacion", "dstick_proyecto.fechaInicio", "dstick_proyecto.fechaFin", "dstick_proyecto.estado"];
+$joins = [['tabla' => 'dstick_diccionario', 'col_origen' => 'idCategoria', 'col_destino' => 'id', 'col_mostrar' => 'dstick_diccionario.titulo' ],['tabla' => 'dstick_cliente', 'col_origen' => 'idCliente', 'col_destino' => 'id', 'col_mostrar' => 'dstick_cliente.nombre' ],['tabla' => 'usuarios', 'col_origen' => 'idLiderProyecto', 'col_destino' => 'id', 'col_mostrar' => 'usuarios.nombre' ],];
--- inc/dstickproyecto/tpl/administracion.tpl.php
+++ inc/dstickproyecto/tpl/administracion.tpl.php
@@ -8,7 +8,7 @@
 
     <div id="administrar">
 	<p>Seleccione proyecto y despu&eacute;s d&eacute; click sobre la acci&oacute;n que desee realizar.</p>
-	<table id="tbldatab0d48d5325dfbacbe15be19c115a681d8" data-toggle="table" data-url="/json/dstickproyecto/?joins=YToxOntpOjA7YTo0OntzOjU6InRhYmxhIjtzOjg6InVzdWFyaW9zIjtzOjEwOiJjb2xfb3JpZ2VuIjtzOjE1OiJpZExpZGVyUHJveWVjdG8iO3M6MTE6ImNvbF9kZXN0aW5vIjtzOjI6ImlkIjtzOjExOiJjb2xfbW9zdHJhciI7czoxNToidXN1YXJpb3Mubm9tYnJlIjt9fQ==" data-pagination="true"
+	<table id="tbldatab0d48d5325dfbacbe15be19c115a681d8" data-toggle="table" data-url="/json/dstickproyecto/?joins=" data-pagination="true"
 	   data-method="post" data-side-pagination="server" data-page-list="[5, 10, 20, 50, 100, 200, 300, 400, 500, 1000, 5000, 10000]" data-toolbar="#toolbar-bootstrapTable"
 	   data-search="true" data-striped="true" data-height="500" data-cache="true" data-show-refresh="true"
 	   data-click-to-select="true" data-select-item-name="id" data-show-toggle="true" data-show-columns="true" >
@@ -16,14 +16,14 @@
 	<tr>
 		<th data-field="null" data-radio="true"></th>
 		<th data-field="id" data-sortable="true">No</th>
-		<th data-field="FKidLiderProyecto" data-sortable="true" data-visible="true">IdLiderProyecto</th>
-		<th data-field="FKidCategoria" data-sortable="true" data-visible="true">IdCategoria</th>
-		<th data-field="FKidCliente" data-sortable="true" data-visible="true">IdCliente</th>
+		<th data-field="FKidLiderProyecto" data-sortable="true" data-visible="true">LiderProyecto</th>
+		<th data-field="FKidCategoria" data-sortable="true" data-visible="true">Categoría</th>
+		<th data-field="FKidCliente" data-sortable="true" data-visible="true">Cliente</th>
 		<th data-field="nombre" data-sortable="true" data-visible="true">Nombre</th>
-		<th data-field="descripcion" data-sortable="true" data-visible="true">Descripcion</th>
-		<th data-field="fechaCreacion" data-sortable="true" data-visible="true">FechaCreacion</th>
-		<th data-field="fechaInicio" data-sortable="true" data-visible="true">FechaInicio</th>
-		<th data-field="fechaFin" data-sortable="true" data-visible="true">FechaFin</th>
+		<th data-field="descripcion" data-sortable="true" data-visible="true">Descripción</th>
+		<th data-field="fechaCreacion" data-sortable="true" data-visible="true">Fecha de Creación</th>
+		<th data-field="fechaInicio" data-sortable="true" data-visible="true">Fecha de Inicio</th>
+		<th data-field="fechaFin" data-sortable="true" data-visible="true">Fecha de Término</th>
 		<th data-field="estado" data-sortable="true" data-visible="true">Estado</th>
 
 	</tr>
@@ -33,7 +33,7 @@
 		<button type="button" class="btn btn-default" data-toggle="modal" data-target="#crear"><i class="fa fa-plus"></i> Nuevo registro</button>
 		<button type="button" class="btn btn-default" data-toggle="modal" data-target="#frmActualizar"><i class="fa fa-pencil"></i> Modificar</button>
 		<button type="button" class="btn btn-default" data-toggle="modal" data-target="#frmEliminar"><i class="fa fa-trash"></i> Eliminar</button>
-		<a href='/xlsx/dstick_proyecto/?joins=YToxOntpOjA7YTo0OntzOjU6InRhYmxhIjtzOjg6InVzdWFyaW9zIjtzOjEwOiJjb2xfb3JpZ2VuIjtzOjE1OiJpZExpZGVyUHJveWVjdG8iO3M6MTE6ImNvbF9kZXN0aW5vIjtzOjI6ImlkIjtzOjExOiJjb2xfbW9zdHJhciI7czoxNToidXN1YXJpb3Mubm9tYnJlIjt9fQ==' class='btn btn-default'><i class='fa fa-download'></i> Exportar</a>
+		<a href='/xlsx/dstickproyecto/?joins=' class='btn btn-default'><i class='fa fa-download'></i> Exportar</a>
 	</div>
     </div>
 
@@ -95,31 +95,31 @@
 							</div>
 						</div>
 
-						<div class="form-group hidden">
+						<div class="form-group">
 							<label for="txtfechaCreacion" class="col-sm-2 control-label">FechaCreacion</label>
 							<div class="col-sm-10">
-								<input id="txtfechaCreacion" name="txtfechaCreacion" type="text" class="form-control" placeholder="fechaCreacion" value="{@pag->ahora}" />
+								<input id="txtfechaCreacion" name="txtfechaCreacion" type="text" class="form-control" placeholder="fechaCreacion" />
 							</div>
 						</div>
 
 						<div class="form-group">
 							<label for="txtfechaInicio" class="col-sm-2 control-label">FechaInicio</label>
 							<div class="col-sm-10">
-								<input id="txtfechaInicio" name="txtfechaInicio" type="text" class="form-control datetime" placeholder="fechaInicio" />
+								<input id="txtfechaInicio" name="txtfechaInicio" type="text" class="form-control" placeholder="fechaInicio" />
 							</div>
 						</div>
 
 						<div class="form-group">
 							<label for="txtfechaFin" class="col-sm-2 control-label">FechaFin</label>
 							<div class="col-sm-10">
-								<input id="txtfechaFin" name="txtfechaFin" type="text" class="form-control datetime" placeholder="fechaFin" />
+								<input id="txtfechaFin" name="txtfechaFin" type="text" class="form-control" placeholder="fechaFin" />
 							</div>
 						</div>
 
-						<div class="form-group hidden">
+						<div class="form-group">
 							<label for="txtestado" class="col-sm-2 control-label">Estado</label>
 							<div class="col-sm-10">
-								<input id="txtestado" name="txtestado" type="text" class="form-control" placeholder="estado" value="Activo" />
+								<input id="txtestado" name="txtestado" type="text" class="form-control" placeholder="estado" />
 							</div>
 						</div>
 
