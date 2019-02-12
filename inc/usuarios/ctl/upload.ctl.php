1303a327e3927d72c3b2b4942df5e552bbdbc20f
Mauricio Vera <mail@mauriciovera.com>
2019-02-11 19:26:35 -0600

    Initial commit

--- inc/usuarios/ctl/upload.ctl.php
+++ inc/usuarios/ctl/upload.ctl.php
@@ -1,36 +1,5 @@
 <?php
 $this->establecerTitulo("Upload");
-$script = <<<EOM
-$(function () {
-    'use strict';
-    var url = "/upload/usuarios/";
-    $('#fileuploadlogotipo').fileupload({
-        url: url,
-        dataType: 'json',
-        filesContainer: '#filesdisponibles',
-		dropZone: $('#dropzone'),
-        done: function (e, data) {
-			console.log("LISTO");
-			$.each(data.result.files, function (index, file) {
-                $('<p/>').text(file.name).appendTo('#filesdisponibles');
-            });
-		},
-        progressall: function (e, data) {
-            var progress = parseInt(data.loaded / data.total * 100, 10);
-            $('#progress .progress-bar').css(
-                'width',
-                progress + '%'
-            );
-        }
-    }).prop('disabled', !$.support.fileInput)
-        .parent().addClass($.support.fileInput ? undefined : 'disabled');
-
-    $.getJSON(url, function (files) {
-        console.log(files);
-    });
-});
-EOM;
-//$this->agregarScript($script);
 
 $uploadCampo = \DepotServer\DSUploader::generar("logotipo", "/upload/usuarios/");
 $this->ev("campoCarga", $uploadCampo["campo"]);
--- inc/usuarios/tpl/upload.tpl.php
+++ inc/usuarios/tpl/upload.tpl.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		
 		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-			{@pag->campoCarga}			
+			{@pag->campoCarga}		
 		</div>
 		
 		<div class="col-md-4 col-md-offset-4">
